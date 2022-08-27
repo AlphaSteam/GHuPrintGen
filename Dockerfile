7e5841250637bc435233290b2d6cec8d3c9c6c75
AlphaSteam <sebastian.alfaro@ing.uchile.cl>
2022-08-27 07:59:33 -0400

    ⚗️ main First test iteration

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3
+
+COPY requirements.txt /requirements.txt
+
+RUN pip install -r /requirements.txt
+
+COPY script.py /script.py
+
+CMD ["python", "/script.py"] 
---
+++ script.py
@@ -0,0 +1,22 @@
+from ghapi.all import GhApi
+
+def get_logs():
+
+    repository_from_context = context_github.repository.split('/')
+    owner = repository_from_context[0]
+    repo = repository_from_context[1]
+    
+    api = GhApi(owner=owner,repo=repo,token=github_token())
+
+    run_id = context_github.run_id
+
+    jobs = api.actions.list_jobs_for_workflow_run_attempt(run_id=run_id)
+
+    print(jobs)
+    
+def main():
+    get_logs()
+
+if __name__ == "__main__":
+    main()
+
