9995c6d042965410cc2d65df4f2f96f181cd9bda
AlphaSteam <sebastian.alfaro@ing.uchile.cl>
2022-08-27 08:28:18 -0400

    ⚗️ main Experimental fix

--- script.py
+++ script.py
@@ -1,19 +1,22 @@
-from ghapi.all import GhApi
+import ghapi.all as ghapi
 
 def get_logs():
 
-    repository_from_context = env_github.repository.split('/')
+    repository_from_context = ghapi.env_github.repository.split('/')
     owner = repository_from_context[0]
     repo = repository_from_context[1]
     
-    api = GhApi(owner=owner,repo=repo,token=github_token())
+    api = ghapi.GhApi(owner=owner,repo=repo,token=github_token())
 
-    current_job_id = env_github.job
+    current_job_id = ghapi.env_github.job
+
+    current_job_logs = api.actions.download_job_logs_for_workflow_run(current_job_id)
+
+    print(current_job_logs)
     #run_id = context_github.run_id
 
     #jobs = api.actions.list_jobs_for_workflow_run_attempt(run_id=run_id)
 
-    print(jobs)
 
 def main():
     get_logs()
