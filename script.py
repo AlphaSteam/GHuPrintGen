1b5cfc09e1dcb87b6c25084e6dd664036ba6e92a
AlphaSteam <sebastian.alfaro@ing.uchile.cl>
2022-08-27 08:40:07 -0400

    🔊 main Added logs

--- script.py
+++ script.py
@@ -3,12 +3,20 @@ import ghapi.all as ghapi
 def get_logs():
 
     repository_from_context = ghapi.env_github.repository.split('/')
+
+    print("repository_from_context", repository_from_context)
+
     owner = repository_from_context[0]
+    print("owner", owner)
+
     repo = repository_from_context[1]
+    print("repo", repo)
+
     
     api = ghapi.GhApi(owner=owner,repo=repo,token=ghapi.github_token())
 
     current_job_id = ghapi.env_github.job
+    print("current_job_id", current_job_id)
 
     current_job_logs = api.actions.download_job_logs_for_workflow_run(current_job_id)
 
