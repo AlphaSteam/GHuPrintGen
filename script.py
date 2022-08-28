f8bf45d96d6feff759fd73380418965ac51f7891
AlphaSteam <sebastian.alfaro@ing.uchile.cl>
2022-08-28 06:09:45 -0400

    ✨ main Added functions for getting job_id

--- script.py
+++ script.py
@@ -1,39 +1,49 @@
 import ghapi.all as ghapi
 import os
 
+def get_job_by_name(job_name, job_list):
+
+    for _, job_obj in enumerate(job_list):
+        if job_obj["name"] == job_name:
+            return job_obj
+
+
+def get_job_id(api, job_name, run_id):
+
+    all_jobs = api.actions.list_jobs_for_workflow_run(run_id=run_id)["jobs"]
+
+    return get_job_by_name(job_name, all_jobs)["id"]
+
+    
 def get_logs():
 
     repository = os.environ['INPUT_REPOSITORY'].split("/")
     
-    print("repository", repository)
-
     owner = repository[0]
-    print("owner", owner)
 
     repo = repository[1]
-    print("repo", repo)
 
     token = os.environ['INPUT_GITHUB-TOKEN']
 
     api = ghapi.GhApi(owner=owner, repo=repo, token=token)
 
-    current_job_id = ghapi.env_github.job
-    print("current_job_id", current_job_id)
+    job_name = ghapi.env_github.job
 
-    try:
-        current_job_logs = api.actions.download_job_logs_for_workflow_run(job_id=current_job_id)
-        print("current_job_logs", current_job_logs)
+    run_id = ghapi.env_github.run_id
 
-    except Exception as e:
-        print("Exception", e)
+    current_job_id = get_job_id(api, job_name, run_id)
+    
+    current_job_logs = api.actions.download_job_logs_for_workflow_run(job_id=current_job_id)
+    
+    print("current_job_logs", current_job_logs)
 
-    #run_id = context_github.run_id
-
-    #jobs = api.actions.list_jobs_for_workflow_run_attempt(run_id=run_id)
-
+    
 
 def main():
-    get_logs()
+    try:
+        get_logs()
+    except Exception as e:
+        print("Exception", e)
 
 if __name__ == "__main__":
     main()
